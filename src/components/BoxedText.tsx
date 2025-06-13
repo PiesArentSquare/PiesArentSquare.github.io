7ed6f4bb279de30726ebd350f6ced3e85d3b290b
PiesArentSquare <jjhh12@outlook.com>
2025-06-13 19:00:24 -0400

    added placeholder about me section

--- src/components/BoxedText.tsx
+++ src/components/BoxedText.tsx
@@ -7,22 +7,26 @@ interface BoxedTextProps {
     className?: string
     backgroundColor?: string
     blendMode?: string
+    direction?: 'left' | 'right'
 }
 
-const BoxedText = ({as: Tag = 'h2', imgRef, content, className, backgroundColor, blendMode}: BoxedTextProps) => {
+const BoxedText = ({as: Tag = 'h2', imgRef, content, className, backgroundColor, blendMode, direction = 'right'}: BoxedTextProps) => {
     const {ref, imgSize, imgPos, recalculate} = imgRef ? useImageAlignment(imgRef) : {}
 
-    return <div>
-        <div className={`relative inline-block ${className}`}>
+    const boxMargin = direction == 'left' ? 'ml-4' : 'mr-4'
+    const textPadding = direction == 'left' ? 'pl-4' : 'pr-4'
+
+    return <div className={className}>
+        <div className={`relative inline-block ${boxMargin}`}>
             {/* text */}
-            <Tag className='relative z-20 pr-4 pb-5 mb-4'>{content}</Tag>
+            <Tag className={`relative z-20 ${textPadding} pb-9`}>{content}</Tag>
             {/* box */}
             <div className='absolute top-[20%] left-12 bottom-1 -right-4 border-2 border-brand-primary'></div>
             {/* stroke background for blending */}
             <div className={`absolute inset-0 bg-clip-text pr-4 pb-4 z-5 ${backgroundColor}`} style={{WebkitTextStroke: '2rem transparent'}}>{content}</div>
             {/* stroke image background */}
             {imgRef
-                ? <div ref={ref} className={`absolute inset-0 bg-clip-text pr-4 pb-4 z-10 ${blendMode}`} style={{WebkitTextStroke: '2rem transparent', backgroundImage: `url("${imgRef.current?.src}")`, backgroundSize: imgSize, backgroundPosition: imgPos}}>{content}</div>
+                ? <div ref={ref} className={`absolute inset-0 bg-clip-text ${textPadding} pb-9 z-10 ${blendMode}`} style={{WebkitTextStroke: '2rem transparent', backgroundImage: `url("${imgRef.current?.src}")`, backgroundSize: imgSize, backgroundPosition: imgPos}}>{content}</div>
                 : <></>
             }
         </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,9 +6,10 @@ import BoxedText from '../components/BoxedText'
 
 const Home = () => {
     const background = useRef<HTMLImageElement>(null)
-    const blendMode = 'mix-blend-difference', backgroundColor = 'bg-amber-700'
+    const blendMode = 'mix-blend-difference', backgroundColor = 'bg-blue-700'
 
     return <div className='text-brand-text bg-brand-background text-xl lowercase'>
+        <a href='#hero' className='absolute left-0 top-0 bg-brand-background text-brand-text py-2 px-4 z-50 -translate-y-full focus:translate-0 focus:outline-brand-background focus:border-2 focus:border-brand-primary focus:outline-4 transition border-dashed'>Skip to main content</a>
         <nav className='fixed backdrop-blur-sm inset-x-0 text-brand-background z-40'>
             <ul className='flex gap-16 mx-16 py-6 items-center'>
                 <li className='grow'><img src={logo} alt='PiesArentSquare'/></li>
@@ -27,10 +28,16 @@ const Home = () => {
                     <h2 className='inline-block text-5xl font-extrabold align-bottom'>Hey,</h2>
                 </div>
                 <BoxedText as='h1' imgRef={background} content="I'm Jeremiah" className='text-[144px]/[.8em] font-extrabold mb-12' blendMode={blendMode} backgroundColor={backgroundColor} />
-                <BoxedText as='p' imgRef={background} content='I build full-stack apps with clean code for smooth dev and user experiences alike' className='text-2xl font-bold max-w-[400px] ml-20' blendMode={blendMode} backgroundColor={backgroundColor} />
+                <BoxedText as='p' imgRef={background} content='I build full-stack apps with clean code for smooth dev and user experiences alike' className='text-2xl font-bold max-w-[420px] ml-20' blendMode={blendMode} backgroundColor={backgroundColor} />
             </div>
         </section>
-        <section id='about-me'>
+        <section id='about-me' className='flex justify-end py-48 px-[10%]'>
+            <BoxedText content='About Me' backgroundColor='bg-brand-background' className='text-5xl font-extrabold mr-12' />
+            <div className='max-w-[60ch] flex flex-col gap-12 mt-3'>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas et  ligula nec velit volutpat imperdiet ut at metus. Praesent gravida tortor quis augue euismod mollis. Suspendisse potenti. Pellentesque quis elit  libero. Phasellus semper risus non nunc imperdiet, vitae luctus metus  vulputate. Curabitur aliquet diam sed ligula maximus laoreet non in  nunc. Donec turpis nisi, tincidunt vitae nisl tincidunt, dapibus  placerat ligula. Etiam a malesuada diam.</p>
+                <p>Sed nec lorem dolor. Nulla diam elit, commodo non fringilla vitae,  viverra vitae est. Duis hendrerit iaculis arcu vel malesuada. Praesent  euismod nulla sed velit posuere, in varius ante eleifend. Donec dui  metus, efficitur a felis sed, tincidunt tempor enim. Cras sit amet  tincidunt sapien. Morbi molestie magna vitae felis placerat, sed  sagittis lacus tempus. Fusce volutpat odio ac lorem vehicula posuere. Ut ac est in sem auctor condimentum. Cras a ultrices lacus.</p>
+                <p>Aliquam sollicitudin nulla at felis vulputate dignissim. Maecenas sit  amet tempus est. Etiam porttitor ipsum id mauris scelerisque, ut  vestibulum erat blandit. Aenean et orci eu leo dictum accumsan sit amet  eu metus. Phasellus id dui eget erat condimentum molestie. Donec sit  amet mi ex. Vivamus sit amet massa a eros finibus volutpat. Donec nec  elit nec justo laoreet lacinia. Nullam bibendum ligula a neque suscipit  elementum. Mauris vel hendrerit dolor. Sed eu commodo felis. Vestibulum  ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia  curae; Aenean fringilla nunc felis.</p>
+            </div>
         </section>
     </div>
 }
